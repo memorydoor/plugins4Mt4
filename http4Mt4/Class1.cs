2fbea2af8b9d2ca90cf8e0c7d5a39d96d9812b66
ycheng <yong.cheng.in@gmail.com>
2016-09-09 22:30:35 -0400

    Created http4Mt4 plugin.

---
+++ http4Mt4/Class1.cs
@@ -0,0 +1,48 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+namespace http4Mt4
+{
+    public class Class1
+    {
+
+        //[DllExport("GetMessageFromQueue", CallingConvention = CallingConvention.StdCall)]
+        //[return: MarshalAs(UnmanagedType.LPWStr)]
+        //public static string GetMessageFromQueue([MarshalAs(UnmanagedType.LPWStr)] string queueName)
+
+        [DllExport("httpGetString", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string httpGetString([MarshalAs(UnmanagedType.LPWStr)]String url)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                var response = httpClient.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // by calling .Result you are performing a synchronous call
+                    var responseContent = response.Content;
+
+                    // by calling .Result you are synchronously reading the result
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    return responseString;
+                    //Console.WriteLine(responseString);
+                }
+                else {
+                    return "error";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                string checkResult = "Error " + ex.ToString();
+                httpClient.Dispose();
+                return checkResult;
+            }
+        }
+    }
+}
---
+++ http4Mt4UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using http4Mt4;
+
+namespace http4Mt4UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = Class1.httpGetString("http://localhost:8081/mt4Rest/getFormingXabcds?symbol#");
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
