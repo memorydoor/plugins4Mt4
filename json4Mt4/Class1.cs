3d5e59f5ad8b9b79fdfb9a1de26ccfe9329094c2
ycheng <yong.cheng.in@gmail.com>
2016-09-14 21:33:22 -0400

    MT4: added json4Mt4.

---
+++ json4Mt4/Class1.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json.Linq;
+using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using log4net;
+using log4net.Repository.Hierarchy;
+using log4net.Layout;
+using log4net.Appender;
+using log4net.Core;
+
+namespace json4Mt4
+{
+    public class Class1
+    {
+
+        static ILog log;
+
+        static Class1()
+        {
+            Setup();
+            log4net.Config.BasicConfigurator.Configure();
+            log = log4net.LogManager.GetLogger(typeof(Class1));
+        }
+
+        public static void Setup()
+        {
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            PatternLayout patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%date [%thread] %-5level %logger - %message%newline";
+            patternLayout.ActivateOptions();
+
+            RollingFileAppender roller = new RollingFileAppender();
+            roller.AppendToFile = true;
+
+            roller.File = @"D:/Log.txt";
+            roller.Layout = patternLayout;
+            roller.MaxSizeRollBackups = 5;
+            roller.MaximumFileSize = "1GB";
+            roller.RollingStyle = RollingFileAppender.RollingMode.Size;
+            roller.StaticLogFileName = true;
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            MemoryAppender memory = new MemoryAppender();
+            memory.ActivateOptions();
+            hierarchy.Root.AddAppender(memory);
+
+            hierarchy.Root.Level = Level.Info;
+            hierarchy.Configured = true;
+        }
+
+
+        [DllExport("jsonCount", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string jsonCount(
+            [MarshalAs(UnmanagedType.LPWStr)]string json,
+            [MarshalAs(UnmanagedType.LPWStr)]string path) {
+            try
+            {
+                JObject o = JObject.Parse(json);
+                log.Info(path);
+                IEnumerable<JToken> acme = o.SelectTokens(path);
+
+                long count = acme.LongCount();
+                
+                log.Info(count);
+                return count.ToString();
+            }
+            catch (Exception e)
+            {
+                log.Info(e);
+
+                ShowDLLException(e);    
+                return "exception happenned";
+            }
+        }
+
+        private static void ShowDLLException(Exception e)
+        {
+            MessageBox.Show(e.Message, "DLL exception", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+
+
+        [DllExport("getSimpleJson", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string getSimpleJson(
+            [MarshalAs(UnmanagedType.LPWStr)]string json,
+            [MarshalAs(UnmanagedType.LPWStr)]string path,
+            [MarshalAs(UnmanagedType.LPWStr)]string index)
+        {
+            try
+            {
+                JObject o = JObject.Parse(json);
+                log.Info("path:" + path);
+                log.Info("index:" + index);
+                IEnumerable<JToken> acme = o.SelectTokens(path);
+                string result = acme.ToList()[Int32.Parse(index)].ToString();
+                log.Info(result);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                log.Info(e);
+
+                ShowDLLException(e);    
+                return "exception happenned";
+            }
+        }
+
+        [DllExport("getString", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string getString(
+            [MarshalAs(UnmanagedType.LPWStr)]string json,
+            [MarshalAs(UnmanagedType.LPWStr)]string path)
+        {
+            JObject o = JObject.Parse(json);
+            JToken acme = o.SelectToken(path);
+            string result =  acme.ToString();
+
+            return "" + result;
+        }
+
+        [DllExport("getInt", CallingConvention = CallingConvention.StdCall)]
+        public static int getInt(
+            [MarshalAs(UnmanagedType.LPWStr)]string json,
+            [MarshalAs(UnmanagedType.LPWStr)]string path)
+        {
+            JObject o = JObject.Parse(json);
+            JToken acme = o.SelectToken(path);
+            int result = Int32.Parse(acme.ToString());
+            return result;
+        }
+
+    }
+}
---
+++ json4Mt4UnitTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using json4Mt4;
+
+namespace json4Mt4UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string json = @"{
+                'count': 2,
+                'xabcds': [
+                {
+                    'symbol': 'USDJPY',
+                    'period': 240,
+                    'description': 'test1'
+                },
+                {
+                    'symbol': 'USDJPY',
+                    'period': 240,
+                    'description': 'test2'
+                },
+                {
+                    'symbol': 'USDJPY',
+                    'period': 60,
+                    'description': 'test2'
+                }
+                ]
+            }";
+
+            String xpath = "$.xabcds[?(@.symbol == 'USDJPY' && @.period == 240)]";
+            long result = Class1.jsonCount(json, xpath);
+            Assert.AreEqual(2, result);
+
+            string path = "$.xabcds[?(@.symbol == 'USDJPY' && @.period == 240)]";
+            string result1 = Class1.getSimpleJson(json, path, 1);
+            //Assert.AreEqual(2, result1);
+
+            string json1 = @"               {
+                    'symbol': 'USDJPY',
+                    'period': 60,
+                    'description': 'test2'
+                }";
+
+            path = "$.symbol";
+            string result2 = Class1.getString(json1, path);
+            Assert.AreEqual("USDJPY", result2);
+            Assert.AreEqual(60, Class1.getInt(json1, "$.period"));
+        }
+
+        [TestMethod]
+        public void testMethod2() {
+            string jsonStr = "{\"count\":4,\"xabcds\":[{\"id\":-1909669210,\"symbol\":\"USDJPY\",\"period\":240,\"extremumType\":\"high\",\"x\":\"2016.07.27 00:00:00\",\"a\":\"2016.08.02 16:00:00\",\"b\":\"2016.08.08 12:00:00\",\"c\":\"2016.08.16 12:00:00\",\"perfectD\":104.168,\"d\":\"NA\"},{\"id\":476840296,\"symbol\":\"USDJPY\",\"period\":240,\"extremumType\":\"high\",\"x\":\"2016.07.25 00:00:00\",\"a\":\"2016.08.02 16:00:00\",\"b\":\"2016.08.08 12:00:00\",\"c\":\"2016.08.16 12:00:00\",\"perfectD\":104.168,\"d\":\"NA\"},{\"id\":954892012,\"symbol\":\"USDJPY\",\"period\":240,\"extremumType\":\"high\",\"x\":\"2016.07.21 00:00:00\",\"a\":\"2016.08.02 16:00:00\",\"b\":\"2016.08.08 12:00:00\",\"c\":\"2016.08.16 12:00:00\",\"perfectD\":104.297,\"d\":\"NA\"},{\"id\":788195411,\"symbol\":\"USDJPY\",\"period\":1440,\"extremumType\":\"high\",\"x\":\"2016.01.29 00:00:00\",\"a\":\"2016.05.03 00:00:00\",\"b\":\"2016.05.30 00:00:00\",\"c\":\"2016.06.24 00:00:00\",\"perfectD\":116.715,\"d\":\"NA\"}]}";
+            String xpath = "$.xabcds[?(@.symbol == 'USDJPY' && @.period == 240)]";
+            int result = Class1.jsonCount(jsonStr, xpath);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
