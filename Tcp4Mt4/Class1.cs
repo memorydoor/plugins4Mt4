3062f8189b020e0ee1a2ccf157d7c5bd3aa5c0f7
ycheng <yong.cheng.in@gmail.com>
2016-10-13 01:03:00 -0400

    Added Tcp4Mt4

---
+++ Tcp4Mt4/Class1.cs
@@ -0,0 +1,75 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp4Mt4
+{
+    public class Class1
+    {
+
+        public static TcpClient tcpclnt;
+        private static string hostStr;
+        private static string portStr;
+
+        [DllExport("TcpConnect", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string TcpConnect(
+             [MarshalAs(UnmanagedType.LPWStr)]string host,
+             [MarshalAs(UnmanagedType.LPWStr)]string port)
+        {
+            hostStr = host;
+            portStr = port;
+            return internalTcpConnect(host, port);
+        }
+
+        private static string internalTcpConnect(string host, string port)
+        {
+            try
+            {
+                tcpclnt = new TcpClient();
+                tcpclnt.Connect(host, int.Parse(port));
+            }
+            catch (Exception e)
+            {
+
+                return "ERROR:" + e.Message.ToString();
+            }
+
+            return "";
+        }
+
+        public static Stream stm;
+
+        [DllExport("TcpGet", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string TcpGet(
+            [MarshalAs(UnmanagedType.LPWStr)]string request) {
+            string result = "";
+            try
+            {
+                stm = tcpclnt.GetStream();
+                StreamWriter sw = new StreamWriter(stm);
+
+                sw.WriteLine("a");
+                sw.Flush();
+
+                result = new StreamReader(stm).ReadLine();
+            }
+            catch (Exception e)
+            {
+                if (!tcpclnt.Connected) {
+                    return internalTcpConnect(hostStr, portStr);
+                }
+                return "ERROR: " + e.Message.ToString();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tcp4Mt4UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tcp4Mt4;
+
+namespace Tcp4Mt4UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1.TcpConnect("127.0.0.1", "11111");
+
+            string result = Class1.TcpGet("hello");
+            Assert.AreEqual("a", result);
+        }
+    }
+}
